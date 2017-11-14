39941b7e863f6f5479a34c171830b4ff07e579ad
Trevor Keller <trevor.keller@gmail.com>
2017-11-14 01:36:13 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from i3pystatus import Status
+
+status = Status()
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+    format="%a %-d %b %I:%M %p",)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
+
+# The battery monitor has many formatting options, see README for details
+
+# This would look like this, when discharging (or charging)
+# ↓14.22W 56.15% [77.81%] 2h:41m
+# And like this if full:
+# =14.22W 100.0% [91.21%]
+#
+# This would also display a desktop notification (via D-Bus) if the percentage
+# goes below 5 percent while discharging. The block will also color RED.
+# If you don't have a desktop notification demon yet, take a look at dunst:
+#   http://www.knopwob.org/dunst/
+#status.register("battery",
+#    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+#    alert=True,
+#    alert_percentage=5,
+#    status={
+#        "DIS": "↓",
+#        "CHR": "↑",
+#        "FULL": "=",
+#    },)
+
+# This would look like this:
+# Discharging 6h:51m
+#status.register("battery",
+#    format="{status} {remaining:%E%hh:%Mm}",
+#    alert=True,
+#    alert_percentage=5,
+#    status={
+#        "DIS":  "Discharging",
+#        "CHR":  "Charging",
+#        "FULL": "Bat full",
+#    },)
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+    path="/media/Valhalla",
+    format="V {avail}G",)
+
+status.register("disk",
+    path="/home/thor",
+    format="~ {avail}G",)
+
+status.register("disk",
+    path="/",
+    format="/ {avail}G",)
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+#status.register("pulseaudio",
+#    format="♪{volume}",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+status.register("mpd",
+    format="{title}{status}{album}",
+    status={
+        "pause": "▷",
+        "play": "▶",
+        "stop": "◾",
+    },)
+
+status.register("alsa",)
+
+## Displays whether a DHCP client is running
+#status.register("runwatch",
+#    name="DHCP",
+#    path="/var/run/dhclient*.pid",)
+
+# Shows the address and up/down state of enp0s31f6. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (enp0s31f6) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+status.register("network",
+    interface="enp0s31f6",
+    hints={'markup': 'pango'},
+    format_up="↘ {bytes_recv}k ↗ {bytes_sent}k {network_graph}",)
+
+status.run()
