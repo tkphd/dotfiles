b3b7e3ab6d16baf98bacbe9ba2d647d688cda895
Trevor Keller <trevor.keller@nist.gov>
2018-02-02 17:03:39 -0500

    locker updates, urxvt font control

--- fuzzylocker.sh
+++ fuzzylocker.sh
@@ -4,10 +4,10 @@
 /usr/bin/import -window root /tmp/screen_locked.png
 
 # Pixellate it 10x
-/usr/bin/convert /tmp/screen_locked.png -blur 0x6 /tmp/screen_locked.png
+/usr/bin/convert /tmp/screen_locked.png -blur 0x8 /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 /usr/bin/i3lock -i /tmp/screen_locked.png
 
 # Turn the screen off after a delay.
-sleep 600; pgrep i3lock && xset dpms force off
+sleep 60; pgrep i3lock && xset dpms force off
