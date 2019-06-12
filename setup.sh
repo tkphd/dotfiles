171e87b8a264d8e0029069152cc73cb7170e8b8c
Trevor Keller <trevor.keller@gmail.com>
2019-06-11 20:09:19 -0400

    safeguarded script

--- setup.sh
+++ setup.sh
@@ -1,54 +1,63 @@
 #!/bin/bash
 
-DIR=$(pwd)
+echo -ne "DANGER! This script will overwrite ~/.bashrc, and other config files.\n\nType \"yes\" to continue: "
+read EULA
 
-# === bash ===
-if [[ -f ${HOME}/.bashrc ]]; then
-    rm ${HOME}/.bashrc
-fi
-ln -s ${DIR}/bash/bashrc ${HOME}/.bashrc
+if [[ "${EULA}" == "yes" || "${EULA}" == "\"yes\"" ]]; then
 
-# === emacs ===
-if [[ -d ${HOME}/.emacs.d ]]; then
-    rm -rf ${HOME}/.emacs.d
-elif [[ -f ${HOME}/.emacs.d ]]; then
-    rm ${HOME}/.emacs.d
-fi
-ln -s ${DIR}/emacs ${HOME}/.emacs.d
+    DIR=$(pwd)
 
-# === gdb ===
-if [[ -f ${HOME}/.gdbinit ]]; then
-    rm ${HOME}/.gdbinit
-fi
-ln -s ${DIR}/gdb/gdbinit ${HOME}/.gdbinit
+    # === bash ===
+    if [[ -f ${HOME}/.bashrc || -L ${HOME}/.bashrc ]]; then
+        rm ${HOME}/.bashrc
+    fi
+    ln -s ${DIR}/bash/bashrc ${HOME}/.bashrc
 
-# === git ===
-if [[ -f ${HOME}/.gitconfig ]]; then
-    rm ${HOME}/.gitconfig
-fi
-ln -s ${DIR}/git/gitconfig ${HOME}/.gitconfig
+    # === emacs ===
+    if [[ -d ${HOME}/.emacs.d ]]; then
+        rm -rf ${HOME}/.emacs.d
+    elif [[ -f ${HOME}/.emacs.d || -L ${HOME}/.emacs.d ]]; then
+        rm ${HOME}/.emacs.d
+    fi
+    ln -s ${DIR}/emacs ${HOME}/.emacs.d
 
-# === i3wm ===
+    # === gdb ===
+    if [[ -f ${HOME}/.gdbinit || -L ${HOME}/.gdbinit ]]; then
+        rm ${HOME}/.gdbinit
+    fi
+    ln -s ${DIR}/gdb/gdbinit ${HOME}/.gdbinit
 
-if [[ -d ${HOME}/.config/i3wm ]]; then
-    rm -rf ${HOME}/.config/i3
-elif [[ -f ${HOME}/.config/i3 ]]; then
-    rm ${HOME}/.config/i3
-fi
-if [[ -f ${HOME}/.Xresources ]]; then
-    rm ${HOME}/.Xresources
-fi
-if [[ -f ${HOME}/.Xdefaults ]]; then
-    rm ${HOME}/.Xdefaults
-fi
-ln -s ${DIR}/i3wm ${HOME}/.config/i3
-ln -s ${DIR}/i3wm/Xresources ${HOME}/.Xresources
-ln -s ${DIR}/i3wm/Xresources ${HOME}/.Xdefaults
+    # === git ===
+    if [[ -f ${HOME}/.gitconfig || -L ${HOME}/.gitconfig ]]; then
+        rm ${HOME}/.gitconfig
+    fi
+    ln -s ${DIR}/git/gitconfig ${HOME}/.gitconfig
 
-# === urxvt ===
-if [[ -d ${HOME}/.urxvt/ext/font-size ]]; then
-    rm -rf ${HOME}/.urxvt/ext/font-size
-elif [[ -f ${HOME}/.urxvt/ext/font-size ]]; then
-    rm ${HOME}/.urxvt/ext/font-size
+    # === i3wm ===
+    if [[ -d ${HOME}/.config/i3 ]]; then
+        rm -rf ${HOME}/.config/i3
+    elif [[ -f ${HOME}/.config/i3 || -L ${HOME}/.config/i3 ]]; then
+        rm ${HOME}/.config/i3
+    fi
+    ln -s ${DIR}/i3wm ${HOME}/.config/i3
+
+    ## === urxvt ===
+    if [[ -d ${HOME}/.urxvt/ext/font-size ]]; then
+        rm -rf ${HOME}/.urxvt/ext/font-size
+    elif [[ -f ${HOME}/.urxvt/ext/font-size || -L ${HOME}/.urxvt/ext/font-size ]]; then
+        rm ${HOME}/.urxvt/ext/font-size
+    fi
+    ln -s ${DIR}/i3wm/urxvt-font-size/font-size ${HOME}/.urxvt/ext/font-size
+
+    ## === Xresources ===
+    if [[ -f ${HOME}/.Xresources || -L ${HOME}/.Xresources ]]; then
+        rm ${HOME}/.Xresources
+    fi
+    if [[ -f ${HOME}/.Xdefaults ]]; then
+        rm ${HOME}/.Xdefaults
+    fi
+    ln -s ${DIR}/i3wm/Xresources ${HOME}/.Xresources
+    ln -s ${DIR}/i3wm/Xresources ${HOME}/.Xdefaults
+else
+    echo "No changes were made."
 fi
-ln -s ${DIR}/i3wm/urxvt-font-size/font-size ~/.urxvt/ext/font-size
