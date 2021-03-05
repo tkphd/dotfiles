143b22ef22a4d9f5a7ebcbfa020d5d65b5bc382a
Trevor Keller <trevor.keller@gmail.com>
2021-03-05 14:00:37 -0500

    line numbers

--- emacs/init.el
+++ emacs/init.el
@@ -38,7 +38,6 @@
 )
 
 (require 'setup-general)
-
 (require 'setup-cedet)
 (require 'setup-editing)
 
@@ -46,6 +45,22 @@
 (volatile-highlights-mode t)
 
 (load-theme 'tsdh-dark)
+
+(require 'display-line-numbers)
+(defcustom display-line-numbers-exempt-modes '(vterm-mode eshell-mode shell-mode term-mode ansi-term-mode)
+  "Major modes on which to disable the linum mode, exempts them from global requirement"
+  :group 'display-line-numbers
+  :type 'list
+  :version "green")
+
+(defun display-line-numbers--turn-on ()
+  "turn on line numbers but excempting certain majore modes defined in `display-line-numbers-exempt-modes'"
+  (if (and
+       (not (member major-mode display-line-numbers-exempt-modes))
+       (not (minibufferp)))
+      (display-line-numbers-mode)))
+
+(global-display-line-numbers-mode)
 
 ;; Put backup files neatly away (https://emacs.stackexchange.com/a/36)
 (let ((backup-dir (getenv "EMACSBD"))
