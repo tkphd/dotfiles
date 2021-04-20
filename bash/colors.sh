be41a508e70cb5a11bb4554640673831a025d476
Trevor Keller <trevor.keller@gmail.com>
2021-04-20 10:15:23 -0400

    More attention-grabbing sudo prompt

---
+++ bash/colors.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#         Color Light Bold  Used for...
+#   Black 0;30m 0;90m 1;30m
+#     Red 0;31m 0;91m 1;31m Timestamp
+#   Green 0;32m 0;92m 1;32m Prompt char
+#  Yellow 0;33m 0;93m 1;33m
+#    Blue 0;34m 0;94m 1;34m Hostname
+# Magenta 0;35m 0;95m 1;35m Dividers
+#    Cyan 0;36m 0;96m 1;36m Pathname
+#    Gray 0;37m 0;97m 1;37m
+
+NONE='\[\e[0m\]'
+DEFAULT='\[\e[0;39m\]'
+
+BLACK='\[\e[0;30m\]'
+RED='\[\e[0;31m\]'
+GREEN='\[\e[0;32m\]'
+YELLOW='\[\e[0;33m\]'
+BLUE='\[\e[0;34m\]'
+MAGENTA='\[\e[0;35m\]'
+CYAN='\[\e[0;36m\]'
+GREY='\[\e[0;37m\]'
+GREYD='\[\e[0;90m\]'
+WHITE='\[\e[0;97m\]'
+
+# === Light ===
+
+REDL='\[\e[0;91m\]'
+GREENL='\[\e[0;92m\]'
+YELLOWL='\[\e[0;93m\]'
+BLUEL='\[\e[0;94m\]'
+MAGENTAL='\[\e[0;95m\]'
+CYANL='\[\e[0;96m\]'
+
+# === Bold ===
+
+BLACKB='\[\e[1;30m\]'
+REDB='\[\e[1;31m\]'
+GREENB='\[\e[1;32m\]'
+BROWNB='\[\e[1;33m\]'
+BLUEB='\[\e[1;34m\]'
+MAGENTAB='\[\e[1;35m\]'
+CYANB='\[\e[1;36m\]'
+GREYB='\[\e[1;37m\]'
