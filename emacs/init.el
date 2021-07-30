7fe3817785959eca075780484bc4a53750f1f077
Trevor Keller <trevor.keller@gmail.com>
2021-07-30 12:09:05 -0400

    clear display-line-numbers error

--- emacs/init.el
+++ emacs/init.el
@@ -46,19 +46,20 @@
 
 (load-theme 'tsdh-dark)
 
-(require 'display-line-numbers)
-(defcustom display-line-numbers-exempt-modes '(vterm-mode eshell-mode shell-mode term-mode ansi-term-mode)
-  "Major modes on which to disable the linum mode, exempts them from global requirement"
-  :group 'display-line-numbers
-  :type 'list
-  :version "green")
+(when (>= emacs-major-version 27)
+  (require 'display-line-numbers)
+  (defcustom display-line-numbers-exempt-modes '(vterm-mode eshell-mode shell-mode term-mode ansi-term-mode)
+    "Major modes on which to disable the linum mode, exempts them from global requirement"
+    :group 'display-line-numbers
+    :type 'list
+    :version "green")
 
-(defun display-line-numbers--turn-on ()
-  "turn on line numbers but excempting certain majore modes defined in `display-line-numbers-exempt-modes'"
-  (if (and
-       (not (member major-mode display-line-numbers-exempt-modes))
-       (not (minibufferp)))
-      (display-line-numbers-mode)))
+  (defun display-line-numbers--turn-on ()
+    "turn on line numbers but excempting certain majore modes defined in `display-line-numbers-exempt-modes'"
+    (if (and
+         (not (member major-mode display-line-numbers-exempt-modes))
+         (not (minibufferp)))
+        (display-line-numbers-mode))))
 
 (global-display-line-numbers-mode)
 
