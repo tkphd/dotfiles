fbbb6a7cfe5da2f04794f03a309f75599bb1c2a1
Trevor Keller <trevor.keller@gmail.com>
2021-09-14 14:10:36 -0400

    flatpak for gimp, paraview

--- bin/paraview.sh
+++ bin/paraview.sh
@@ -2,12 +2,5 @@
 set -e
 ulimit -s unlimited
 export OMP_NUM_THREADS=$(( $(nproc) / 2 ))
-export CONDAPATH=$(readlink -f "${HOME}/.conda/anaconda")
 
-source "${CONDAPATH}/etc/profile.d/conda.sh"
-
-conda activate paraview
-
-paraview 1>/dev/null 2> "${HOME}/log/paraview.log"
-
-conda deactivate
+flatpak run org.paraview.ParaView 1>/dev/null 2> "${HOME}/log/paraview.log"
