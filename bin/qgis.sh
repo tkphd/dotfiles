9c46c672ff3dc68018adb0e5fc66452691fc31cd
Trevor Keller <trevor.keller@gmail.com>
2021-09-15 09:05:26 -0400

    flatpak does not play nice with filesystems

---
+++ bin/qgis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ulimit -s unlimited
+export OMP_NUM_THREADS=$(( nproc / 2 ))
+
+CONDAPATH="${HOME}/.conda/anaconda"
+source "${CONDAPATH}/etc/profile.d/conda.sh"
+
+conda activate apps
+
+qgis
+
+conda deactivate
