5b6d63e46f053fb2877318c15e8eee9a8b82dda7
Trevor Keller <trevor.keller@gmail.com>
2022-01-03 11:42:31 -0500

    cleaner output

--- bin/fixnames.sh
+++ bin/fixnames.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # Remove spaces from filenames
 
+D=""
+
 for T in d f; do
     find . -type $T | \
     while read X; do
         if [[ -e "$X" ]]; then
             A=$(basename "$X")
             B=$(dirname "$X")
-            O=$(echo "${A}" | tr -s ' ,()[]' '------')
+            O=$(echo "${A}" | tr -s ' ,()[]&' '------')
             O="${O/-./.}"
+            if [[ "${B}" != "${D}" ]]; then
+                D="${B}"
+                echo "${D}/:"
+            fi
             if [[ "${A}" != "${O}" ]]; then
-                echo "Rename ${B}/ ${A} --> ${O}"
+                echo "  ${A} --> ${O}"
                 mv "${B}/${A}" "${B}/${O}"
             fi
         fi
