48d553da46d1f52f29b31acdf137870b2b5b8ac9
Trevor Keller <trevor.keller@gmail.com>
2022-03-03 12:11:39 -0500

    update conda loader

--- bash/env.sh
+++ bash/env.sh
@@ -13,19 +13,20 @@
 # === Borg ===
 export BORG_RSH="ssh -i ${HOME}/.ssh/danger_borg_rsa"
 # === Conda ===
-function condaload {
-    if [[ -a "${HOME}/.conda/anaconda" ]]; then
-        export CONDAPATH=$(readlink -f "${HOME}/.conda/anaconda")
-        __conda_setup=$("${CONDAPATH}/bin/conda" shell.bash hook 2> /dev/null)
-        if [[ $? -eq 0 ]]; then
-            eval "${__conda_setup}"
+if [[ -a "${HOME}/.conda/anaconda" ]]; then
+    export CONDAPATH=$(readlink -f "${HOME}/.conda/anaconda")
+    __conda_setup="$('${CONDAPATH}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${CONDAPATH}/etc/profile.d/conda.sh" ]; then
+            . "${CONDAPATH}/etc/profile.d/conda.sh"
         else
-            [[ -f "${CONDAPATH}/etc/profile.d/conda.sh" ]] && \
-                source "${CONDAPATH}/etc/profile.d/conda.sh"
+            export PATH="${CONDAPATH}/bin:$PATH"
         fi
-        unset __conda_setup
     fi
-}
+    unset __conda_setup
+fi
 # === CUDA ===
 [[ -d "${HOME}/.dotfiles/local" && -f "${HOME}/.dotfiles/local/cudarch.sh" ]] && \
     source "${HOME}/.dotfiles/local/cudarch.sh"
