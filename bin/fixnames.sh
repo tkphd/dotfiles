89ac9f727e9d5738db27d5ff39e075f7dc09d677
Trevor Keller <trevor.keller@gmail.com>
2022-06-10 09:37:05 -0400

    better Plasma integration

--- bin/fixnames.sh
+++ bin/fixnames.sh
@@ -10,6 +10,7 @@ for T in d f; do
             LEAF=$(basename "$X")  # filename
             STEM=$(dirname "$X")  # directory name
             BUD=$(echo "${LEAF}" | tr -s ' ,()[]&!' '------')  # replace special chars
+            BUD="${BUD/\'/}"   # remove '
             BUD="${BUD/-./.}"  # remove trailing '-'
             BUD="${BUD#-}"  # remove leading '-'
             if [[ "${STEM}" != "${BASE}" ]]; then
