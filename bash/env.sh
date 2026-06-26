6c65c793062a31899f212ee6b912f5c59f5edfed
Trevor Keller <trevor@fluidnumerics.com>
2026-06-26 13:48:08 -0400

    Cleanup for FN

--- bash/env.sh
+++ bash/env.sh
@@ -10,20 +10,8 @@ fi
 # === Local Binaries ===
 [[ -d "${HOME}/bin" ]] && [[ ! $PATH =~ .*/$USER/bin* ]] && \
     export PATH="${HOME}/bin:${PATH}"
-# === Snap Binaries ===
-[[ -d "/snap/bin" ]] && [[ ! $PATH =~ .*/snap* ]] && \
-    export PATH="/snap/bin:${PATH}"
-# === AMGX ===
-AMGX_DIR=/toolbox/${USER}/opt/amgx
-[ -d "${AMGX_DIR}" ] && \
-    export AMGX_DIR
-# === BeeGFS ===
-[[ -d /opt/beegfs ]] && [[ ! $PATH =~ .*/opt/beegfs* ]] && \
-    export PATH="${PATH}:/opt/beegfs/sbin"
-# === Borg ===
-export BORG_RSH="ssh -i ${HOME}/.ssh/danger_borg_rsa"
 # === Emacs ===
-export EMACSD="/tmp/${USER}/emacs"
+export EMACSD="~/.cache/emacs"
 export EMACSBD="${EMACSD}/backups"
 export EMACSSD="${EMACSD}/saves"
 if [[ ! -d "${EMACSD}" ]]; then
@@ -36,12 +24,6 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 # === GITHUB ===
 [[ -f "${HOME}/.github" ]] && \
     source "${HOME}/.github" # personal access token(s)
-# === Haskell ===
-[[ -f "${HOME}/.ghcup/env" ]] && \
-    source "${HOME}/.ghcup/env" # ghcup-env
-# === KDE ===
-# export KDE_FULL_SESSION=false
-# export KDEWM=/usr/bin/i3
 # === less ===
 # termcap  terminfo  effect
 # ks       smkx      make the keypad send commands
@@ -68,88 +50,9 @@ LESS_TERMCAP_ZV=$(tput rsubm)                            && export LESS_TERMCAP_
 LESS_TERMCAP_ZO=$(tput ssupm)                            && export LESS_TERMCAP_ZO
 LESS_TERMCAP_ZW=$(tput rsupm)                            && export LESS_TERMCAP_ZW
 GROFF_NO_SGR=1                                           && export GROFF_NO_SGR
+# === LESS ===
 [[ $(which pygmentize) != "" ]] && \
     export LESSOPEN="| pygmentize -g %s"
-# === Modules ===
-if [[ -f /etc/profile.d/lmod.sh ]]; then
-    . /etc/profile.d/lmod.sh
-    [ -d "${HOME}/research/modules/modulefiles" ] && \
-        module use "${HOME}/research/modules/modulefiles"
-fi
-# === MOOSE ===
-[[ -d /toolbox/tnk10/src/moose ]] && \
-    export MOOSE_DIR=/toolbox/tnk10/src/moose
-# === MMSP ===
-export MMSP_PATH="${HOME}/research/projects/mmsp"
-[[ ! $PATH =~ .*mmsp* ]] && \
-    export PATH="${PATH}:${MMSP_PATH}/utility"
-# === Nix ===
-[ -e "${HOME}/.nix-profile" ] && \
-    export PATH="${PATH}:${HOME}/.nix-profile/bin"
-# === Node ===
-if [[ -d "${HOME}/.nvm" ]]; then
-    export NVM_DIR="${HOME}/.nvm"
-    [ -s "${NVM_DIR}/nvm.sh" ] && \
-        source "${NVM_DIR}/nvm.sh"  # This loads nvm
-    [ -s "${NVM_DIR}/bash_completion" ] && \
-        source "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
-fi
-# === Nvidia ===
-# if [[ $(hostname -s) == huginn ]]; then
-#     export __NV_PRIME_RENDER_OFFLOAD=1
-#     export __GLX_VENDOR_LIBRARY_NAME=nvidia
-# fi
-# === OpenMPI ===
-# export OMPI_MCA_opal_cuda_support="false"  # false by default
-# export OMPI_MCA_pml="ucx"
-# export OMPI_MCA_osc="ucx"
-# export UCX_MEMTYPE_CACHE="n"
-# === OpenSCAD plugins ===
-if [[ -d "${HOME}/repositories/dotSCAD" ]]; then
-    DOTSCADPATH="${HOME}/repositories/dotSCAD/src"
-    if [[ -n "${OPENSCADPATH}" ]]; then
-        export OPENSCADPATH="${OPENSCADPATH}:${DOTSCADPATH}"
-    else
-        export OPENSCADPATH="${DOTSCADPATH}"
-    fi
-    unset DOTSCADPATH
-fi
-# === pixi ===
-[[ -a "$HOME/.pixi/bin" ]] && \
-    export PATH="${PATH}:${HOME}/.pixi/bin"
-# === Python ===
-export JUPYTER_PLATFORM_DIRS=1
-export PYTHONPYCACHEPREFIX="/tmp/${USER}/pycache"
-[[ ! -d "${PYTHONPYCACHEPREFIX}" ]] && \
-    mkdir -p "${PYTHONPYCACHEPREFIX}"
-# === Qt ===
-export QT_XKB_CONFIG_ROOT="/usr/share/X11/xkb"
-if [[ -d "/usr/local/lib/plugins/platforms" ]]; then
-   export QT_QPA_PLATFORM_PLUGIN_PATH="/usr/local/lib/plugins/platforms"
-elif [[ -d "/usr/lib/x86_64-linux-gnu/qt5/plugins/platforms" ]]; then
-   export QT_QPA_PLATFORM_PLUGIN_PATH="/usr/lib/x86_64-linux-gnu/qt5/plugins/platforms"
-fi
-# === Ruby ===
-[[ -f "$HOME/.cargo/env" ]] && \
-    . "$HOME/.cargo/env"
-[ -d "${HOME}/.rvm/bin" ] && [[ ! $PATH =~ .*/.rvm* ]] && \
-    export PATH="${PATH}:${HOME}/.rvm/bin"
-[ -s "$HOME/.rvm/scripts/rvm" ] && \
-    . "$HOME/.rvm/scripts/rvm"
-# # === Singularity ===
-# if [[ -d "/working/${USER}" ]]; then
-#     export SINGULARITY_TMPDIR="/working/${USER}/scratch/singularity/tmp"
-#     export SINGULARITY_CACHEDIR="/working/${USER}/scratch/singularity/cache"
-#     [[ -d "${SINGULARITY_TMPDIR}" ]] || mkdir -p "${SINGULARITY_TMPDIR}"
-#     [[ -d "${SINGULARITY_CACHEDIR}" ]] || mkdir -p "${SINGULARITY_TMPDIR}"
-# fi
-# === SSH ===
-export TMOUT=0  # disable server-side SSH disconnections
-SSH_ASKPASS="/usr/bin/ssh-askpass" && export SSH_ASKPASS
-if [[ -a /usr/bin/keychain ]]; then
-    /usr/bin/keychain $HOME/.ssh/id_ed25519
-    source $HOME/.keychain/$HOSTNAME-sh
-fi
 # === Systemd ===
 # export SYSTEMD_PAGER=  # uncomment to disable systemctl's auto-paging feature
 
--- setup.sh
+++ setup.sh
@@ -33,19 +33,6 @@ if [[ "${DISCLAIMER}" == "yes" || "${DISCLAIMER}" == "\"yes\"" ]]; then
         ln -s "$(pwd)/${f}" "${link}"
     done
 
-    # === emacs ===
-    if [[ -d "${HOME}"/.emacs.d ]]; then
-        rm -rf "${HOME}"/.emacs.d
-    elif [[ -f "${HOME}"/.emacs.d || -L "${HOME}"/.emacs.d ]]; then
-        rm "${HOME}"/.emacs.d
-    fi
-    ln -s "${DIR}"/emacs "${HOME}"/.emacs.d
-
-    # === gdb ===
-    [[ -f "${HOME}"/.gdbinit || -L "${HOME}"/.gdbinit ]] && \
-        rm "${HOME}"/.gdbinit
-    ln -s "${DIR}"/gdb/gdbinit "${HOME}"/.gdbinit
-
     # === git ===
     [[ -f "${HOME}"/.gitconfig || -L "${HOME}"/.gitconfig ]] && \
         rm "${HOME}"/.gitconfig
@@ -55,25 +42,6 @@ if [[ "${DISCLAIMER}" == "yes" || "${DISCLAIMER}" == "\"yes\"" ]]; then
     fi
     ln -s "${DIR}"/git/gitconfig "${HOME}"/.gitconfig
 
-    # === i3wm ===
-    if [[ -d "${HOME}"/.config/i3 ]]; then
-        rm -rf "${HOME}"/.config/i3
-    elif [[ -f "${HOME}"/.config/i3 || -L "${HOME}"/.config/i3 ]]; then
-        rm "${HOME}"/.config/i3
-    fi
-    ln -s "${DIR}"/i3wm "${HOME}"/.config/i3
-
-    # === julia ===
-    if [[ -d "${DIR}"/julia ]]; then
-      if [[ -d "${HOME}"/.julia/config ]]; then
-          rm -r "${HOME}"/.julia/config
-      elif [[ -f "${HOME}"/.julia/config || -L "${HOME}"/.julia/config ]]; then
-          rm "${HOME}"/.julia/config
-      fi
-      mkdir -p "${HOME}"/.julia
-      ln -s "${DIR}"/julia "${HOME}"/.julia/config
-    fi
-
     # === nano ===
     if [[ ! -d "${HOME}/.nano" ]]; then
         NANO_TMP="/tmp/nanorc.zip"
@@ -81,82 +49,6 @@ if [[ "${DISCLAIMER}" == "yes" || "${DISCLAIMER}" == "\"yes\"" ]]; then
 
         git clone --recursive github:scopatz/nanorc "${NANO_DIR}"
     fi
-
-    # === tmux ===
-    [[ ! -d "${HOME}"/.config/tmux ]] && \
-        mkdir -p "${HOME}"/.tmux
-    ln -sf "${DIR}"/tmux/tmux.conf       "${HOME}"/.tmux.conf
-    ln -sf "${DIR}"/tmux/tmux.conf.local "${HOME}"/.tmux.conf.local
-
-    # === urxvt ===
-    if [[ -d "${HOME}"/.urxvt ]]; then
-        rm -rf "${HOME}"/.urxvt
-    elif [[ -f "${HOME}"/.urxvt || -L "${HOME}"/.urxvt ]]; then
-        rm "${HOME}"/.urxvt
-    fi
-    ln -s "${DIR}"/urxvt "${HOME}"/.urxvt
-
-    ## === X session ===
-    [[ -f "${HOME}"/.Xresources || -L "${HOME}"/.Xresources ]] && \
-        rm "${HOME}"/.Xresources
-    ln -s "${DIR}"/x11/Xresources "${HOME}"/.Xresources
-
-    [[ -f "${HOME}"/.Xdefaults || -L "${HOME}"/.Xdefaults ]] && \
-        rm "${HOME}"/.Xdefaults
-    ln -s "${DIR}"/x11/Xresources "${HOME}"/.Xdefaults
-
-    [[ -f "${HOME}"/.xsessionrc || -L "${HOME}"/.xsessionrc ]] && \
-        rm "${HOME}"/.xsessionrc
-    ln -s "${DIR}"/x11/xsessionrc "${HOME}"/.xsessionrc
-
-    # === volantes cursor theme ===
-    mkdir -p "${HOME}"/.local/share/icons
-    rsync -a --quiet "${DIR}"/x11/volantes "${HOME}"/.local/share/icons/
-
-    [[ "$(which xrdb)" != "" ]] && \
-        xrdb -merge "${HOME}"/.Xresources
-
-    # === check dependencies ===
-    DEBPKG="byobu diff-so-fancy direnv emacs-nox exa fzf i3 keychain      \
-            konsole lnav mdp plocate pyflakes3 pygmentize python3-flake8  \
-            rxvt-unicode tig tmux visidata xsel yamllint zathura"
-    for PKG in ${DEBPKG}; do
-        PKG_PRESENT=$(dpkg -l "${PKG}" 2>/dev/null | grep '^ii')
-        [[ "${PKG_PRESENT}" == "" ]] && \
-            echo "Warning: ${PKG} not found!"
-    done
-
-    # === rust utilities ===
-    [[ -f "${HOME}/.cargo/env" ]] && \
-        . "${HOME}/.cargo/env"
-    if [[ ! -d "${HOME}/.cargo" ]]; then
-        curl --proto '=https' --tlsv1.2 -sSf https://static.rust-lang.org/rustup/rustup-init.sh | sh
-    else
-        rustup update
-    fi
-    # h/t Julia Evans <https://jvns.ca/blog/2022/04/12/a-list-of-new-ish--command-line-tools/>
-    source "${HOME}/.cargo/env"
-    cargo install ag choose difftastic du-dust exa git-delta lsd ripgrep sd xsv
-
-    # === node utilities ===
-    if [[ ! -d "${HOME}/.nvm" ]]; then
-        curl -o- https:/raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-        [ -s "${HOME}/.nvm/nvm.sh" ] && \
-            . "${HOME}/.nvm/nvm.sh"  # This loads nvm
-        [ -s "${HOME}/.nvm/bash_completion" ] && \
-            . "${HOME}/.nvm/bash_completion"  # This loads nvm bash_completion
-        nvm install --lts
-        nvm use --lts
-        nvm alias default node
-    fi
-    [ -s "${HOME}/.nvm/nvm.sh" ] && \
-        . "${HOME}/.nvm/nvm.sh"  # This loads nvm
-    [ -s "${HOME}/.nvm/bash_completion" ] && \
-        . "${HOME}/.nvm/bash_completion"  # This loads nvm bash_completion
-    if [[ -d "${HOME}/.npm" ]]; then
-        npm install --global markdownlint-cli2 tldr yarn 2&>/dev/null # don't tell me about audit errors
-    fi
-
 else
     echo "No changes were made."
 fi
